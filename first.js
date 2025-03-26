bde1188d6a0ceaf80b171da27b7ddc340fa549be
ankit-yadav90 <ankityadav73699@gmail.com>
2025-03-26 14:52:51 +0530

    looping condition using

---
+++ first.js
@@ -0,0 +1,10 @@
+let heroes = ["ironman" , "thor" ,"hulk", "shaktiman", "spiderman", "antman"];
+//foor loop
+for (let i = 0; i < heroes.length; i++) {
+console.log(heroes[i]);
+}
+
+//for of
+for(let hero of heroes){
+    console.log(hero);
+}
